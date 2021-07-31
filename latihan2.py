57d113089d2873928619c3760bd093bae178d17c
msadhimb <86710473+msadhimb@users.noreply.github.com>
2021-07-31 19:28:27 +0700

    Create latihan2.py

---
+++ latihan2.py
@@ -0,0 +1,49 @@
+#Luas Segitiga
+
+alas = int(input("Alas Segitiga : "))
+tinggi = int(input("Tinggi Segitiga : "))
+
+luas = alas * tinggi
+print("Luas Segitiga adalah ", luas)
+
+#keliling Segitiga
+
+sisi1 = int(input("Sisi 1 Segitiga : "))
+sisi2 = int(input("Sisi 2 Segitiga : "))
+sisi3 = int(input("Sisi 3 Segitiga : "))
+
+keliling = sisi1 + sisi2 + sisi3
+print("Keliling Segitiga adalah ", keliling)
+
+#Luas Persegi Panjang
+
+panjang = int(input("Panjang : "))
+lebar = int(input("Lebar : "))
+
+luas = panjang * lebar
+print("Luas persegi panjang adalah", luas)
+
+#keliling Persegi Panjang
+
+panjang = int(input("Panjang : "))
+lebar = int(input("Lebar : "))
+
+keliling = 2 * (panjang + lebar)
+print("keliling persegi panjang adalah", keliling)
+
+#luas Lingkaran
+
+r = int(input("Jari - jari : "))
+phi = 3.14
+
+luas = phi * r * r
+print("Luas lingkaran adalah", luas)
+
+#keliling lingkaran
+
+r = int(input("Jari - jari : "))
+phi = 3.14
+
+keliling = 2 * phi * r
+print("keliling lingkaran adalah", keliling)
+
