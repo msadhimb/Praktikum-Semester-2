b866a9dabe4483241bb58612e8d333a7b3d5c096
msadhimb <86710473+msadhimb@users.noreply.github.com>
2021-07-31 19:28:31 +0700

    Create List dan Dictionary.py

---
+++ List dan Dictionary.py
@@ -0,0 +1,37 @@
+# print("Pengaplikasian List")
+# print("Contoh 1 (Menambahkan anggota pada list kosong)")
+# List1 = [] # Membuat list kosong
+# List1.append('Ahmad')
+# List1.append(2212)
+# print("Output : ", List1)
+# print("=================")
+
+# print("Contoh 2 (Menghapus anggota pada list)")
+# List2 = [1, 4, 'U']
+# List2.remove('U') # Cara pertama menghapus list
+# print("Output 1 : ", List2)
+# List2_1 = [1, 4, 'U']
+# del (List2_1[2]) # Cara kedua menghapus list
+# print("Output 2 : ", List2)
+# print("=================")
+
+# print("Contoh 3 (Mencari anggota dalam list)")
+# List3 = [1, 2, 3, 5, 5]
+# x = 5 in List3
+# print("Output : ", x)
+
+print("Pengaplikasian Dictionary")
+print("Contoh 1 (Menambahkan Anggota dictionary)")
+Dict1 = {"A":1, "B":2, "C":3}
+Dict1["D"] = 4
+print("Output : ", Dict1)
+print("=================")
+
+print("Contoh 2 (Memanggil anggota dictionary)")
+Dict2 = {"A":1, "B":2, "C":3}
+print("Output : ", Dict2["A"])
+print("=================")
+
+print("Contoh 3 (Mencari anggota dictionary)")
+Dict3 = {"A":1, "B":2, "C":3}
+print("Output : ", "B" in Dict3)
